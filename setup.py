2c67706c12e18e3c6f304764e61cd1ecc96831cc
Seedmanc <seedmanc@gmail.com>
2026-02-10 12:32:57 +0300

    no idea what this for

--- setup.py
+++ setup.py
@@ -1,27 +1,19 @@
 import os
-
 import pkg_resources
 from setuptools import setup, find_packages
 
 setup(
-    name="clip-interrogator",
-    version="0.6.0",
+    name="clip-interrogator-average",
+    version="0.6.1",
     license='MIT',
-    author='pharmapsychotic',
-    author_email='me@pharmapsychotic.com',
-    url='https://github.com/pharmapsychotic/clip-interrogator',
-    description="Generate a prompt from an image",
+    author='seedmanc',
+    author_email='seedmanc@yahoo.com',
+    url='https://github.com/seedmanc/clip-interrogator-average',
+    description="Generate an average prompt for a set of images",
     long_description=open('README.md', encoding='utf-8').read(),
     long_description_content_type="text/markdown",
     packages=find_packages(),
-    install_requires=[
-        str(r)
-        for r in pkg_resources.parse_requirements(
-            open(os.path.join(os.path.dirname(__file__), "requirements.txt"))
-        )
-    ],
     include_package_data=True,
-    extras_require={'dev': ['pytest']},
     classifiers=[
         'Intended Audience :: Developers',
         'Intended Audience :: Science/Research',
